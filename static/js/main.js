c98dedf89b0b4cebd3c8c1b5e1299fefc1a24b91
Karen Menezes <karen.menezes@gmail.com>
2013-03-16 07:53:54 -0700

    added changes to links etc

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    $("#menu a").each(function() {   
       if (this.href == window.location.href) {
-      $(this).parents("li").children('a').addClass("selectedMenu");
+      $(this).addClass("selectedMenu");
    }
    });
 });
