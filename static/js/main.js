064b0781884413780a52e1afbcb26e36845f50ec
Karen Menezes <karen.menezes@gmail.com>
2013-03-16 10:55:24 -0700

    fixed selected classes for menu, sidebar and anchor links
    landing page of cv needs to have have first anchor styled.. to be done

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
       $(this).addClass("selectedMenu");
    }
    });
+
+   $(".sideBarTitles a").each(function() {   
+      if (this.href == window.location.href) {
+      $(this).addClass("selectedSideBarTitles");
+   }
+   });
+
+   $('.anchorsCv a').click( function(e) {   
+      $('.anchorsCv .selectedAnchors').removeClass('selectedAnchors');
+      $(this).addClass('selectedAnchors');
+   });
+
+
 });
