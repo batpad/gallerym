16651d6a9acc48762a8bf0781620f07fa591fdec
Sanjay B <b@pad.ma>
2013-09-05 15:28:56 +0530

    max size

--- maskara/settings.py
+++ maskara/settings.py
@@ -74,7 +74,7 @@ USE_TZ = True
 MEDIA_ROOT = join(PROJECT_ROOT, 'media')
 FILEBROWSER_DIRECTORY = ""
 
-FILEBROWSER_MAX_UPLOAD_SIZE = 100000
+FILEBROWSER_MAX_UPLOAD_SIZE = 104857600
 FILEBROWSER_VERSIONS_BASEDIR = "auto_thumbnails/"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
