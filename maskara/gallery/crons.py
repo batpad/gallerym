6b2f252e21236d66acd350b99ab55c9c125ca57d
Sanjay B <b@pad.ma>
2014-01-01 22:59:04 +0530

    add cmd to create pdf thumbs

--- maskara/gallery/crons.py
+++ maskara/gallery/crons.py
@@ -1,5 +1,8 @@
 from tasks import create_tiles
 from models import *
+from os.path import join
+from subprocess import check_call
+from django.conf import settings
 
 def make_all_tiles():
     for w in ArtistWorkImage.objects.filter(is_hires=True).filter(is_tiled=False):
@@ -15,4 +18,16 @@ def make_links_http():
                 if not obj.url.startswith("http"):
                     obj.url = "http://%s" % obj.url
                     obj.save()
+
+def make_pdf_images():
+    for p in Publication.objects.all():
+        if not p.image and p.pdf:
+            outpath = p.pdf.path[:-4] + ".jpg"
+            infile = join(settings.MEDIA_ROOT, p.pdf.path)
+            outfile = join(settings.MEDIA_ROOT, outpath)
+            cmd = ["convert", infile, outfile]
+            check_call(cmd)
+            p.image = outpath
+            p.save()
+            print p.title
 
